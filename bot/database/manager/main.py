b7c3e05c84703dfbb176813df441fd27f4be0cc2
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2022-04-19 18:17:36 +0530

    Create main.py

---
+++ bot/database/manager/main.py
@@ -0,0 +1,11 @@
+import json
+from bot.utils.filenames import database
+
+
+with open(database.main, "r", encoding="utf-8") as _file:
+    data = json.load(_file)
+
+prefix = data['prefix']
+token = data['token']
+users = data['users']
+channel_ids = data["channel_ids"]
