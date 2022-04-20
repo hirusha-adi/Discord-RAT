645ce9b35acb5b39571a905d1d347685a61b8166
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2022-04-20 23:06:38 +0530

    Update OnMessage.py

--- src/cogs/OnMessage.py
+++ src/cogs/OnMessage.py
@@ -22,18 +22,9 @@ class OnMessage(commands.Cog, description="Handle the messages sent"):
         if self.client.user == message.author:
             return
 
-        # dummy message for other users
-        if message.content.lower().startswith(main.prefix):
-            await message.reply("This bot is still under development. Stay tuned! Thank you.")
-            return
-
         # Send bot prefix
         if self.client.user.mentioned_in(message):
             await message.reply(f'Hey, My bot prefix is {main.prefix}')
-
-        # Process commands only if in correct channel and requested by possible users
-        if (message.channel.id in main.channel_ids) and (message.author.id in main.users):
-            await self.client.process_commands(message)
 
 
 def setup(client: commands.Bot):
