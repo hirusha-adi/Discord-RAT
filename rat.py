532774026ab1199fc40f22b89fe0580ac519ca24
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2022-04-20 09:11:41 +0530

    bug fixes

--- rat.py
+++ rat.py
@@ -8,29 +8,35 @@ from src.database.manager import main as db_main
 client = commands.Bot(command_prefix=db_main.prefix)
 
 # Load all cogs at startup of the bot
-for filename in os.listdir('./bot/cogs'):
+for filename in os.listdir('./src/cogs'):
     if filename.endswith('.py'):
         try:
-            client.load_extension(f'bot.cogs.{filename[:-3]}')
-            print(f"[+] Loaded: bot.cogs.{filename[:-3]}")
+            client.load_extension(f'src.cogs.{filename[:-3]}')
+            print(f"[+] Loaded: src.cogs.{filename[:-3]}")
         except Exception as excl:
             print(
-                f"[+] Unable to load: bot.cogs.{filename[:-3]}  :  {excl}")
+                f"[+] Unable to load: src.cogs.{filename[:-3]}  :  {excl}")
 
 
 @client.command()
 async def loadex(ctx, extension):
     client.load_extension(
-        f'bot.cogs.{extension if not(str(extension).endswith(".py")) else extension[:-3]}')
+        f'src.cogs.{extension if not(str(extension).endswith(".py")) else extension[:-3]}')
     await ctx.send(f"Loaded cog: {extension}")
 
 
 @client.command()
 async def unloadex(ctx, extension):
     client.unload_extension(
-        f'bot.cogs.{extension if not(str(extension).endswith(".py")) else extension[:-3]}')
+        f'src.cogs.{extension if not(str(extension).endswith(".py")) else extension[:-3]}')
     await ctx.send(f"Un-Loaded cog: {extension}")
 
+
+@client.event
+async def on_message(message):
+    # Process commands only if in correct channel and requested by possible users
+    if (message.channel.id in db_main.channel_ids) and (message.author.id in db_main.users):
+        await client.process_commands(message)
 
 if __name__ == "__main__":
     client.run(db_main.token)
--- src/cogs/OnMessage.py
+++ src/cogs/OnMessage.py
@@ -10,7 +10,7 @@ class OnMessage(commands.Cog, description="Handle the messages sent"):
         self.client = client
 
     @commands.Cog.listener()
-    async def on_command_error(self, message: discord.Message):
+    async def on_message(self, message: discord.Message):
 
         """
         The bot can only be used by `users` and in `channel_ids` only!
@@ -18,22 +18,22 @@ class OnMessage(commands.Cog, description="Handle the messages sent"):
             The bot will reply with a saying the bot is still under development
         """
 
+        # bot self check
         if self.client.user == message.author:
             return
 
+        # dummy message for other users
         if message.content.lower().startswith(main.prefix):
             await message.reply("This bot is still under development. Stay tuned! Thank you.")
-
-        if not (message.channel.id in main.channel_ids):
             return
 
-        if not (message.author.id in main.users):
-            return
-
+        # Send bot prefix
         if self.client.user.mentioned_in(message):
             await message.reply(f'Hey, My bot prefix is {main.prefix}')
 
-        await self.client.process_commands(message)
+        # Process commands only if in correct channel and requested by possible users
+        if (message.channel.id in main.channel_ids) and (message.author.id in main.users):
+            await self.client.process_commands(message)
 
 
 def setup(client: commands.Bot):
