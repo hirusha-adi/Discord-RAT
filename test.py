ada1b1ba7fb6c57fda5e32f083c706ad3e20eb78
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2022-04-20 23:26:53 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,26 +1,5 @@
-import platform
-import socket
-import re
-import uuid
-import psutil
+import pwd
 
-
-def getSystemInfo():
-    try:
-        info = {}
-        info['platform'] = platform.system()
-        info['platform-release'] = platform.release()
-        info['platform-version'] = platform.version()
-        info['architecture'] = platform.machine()
-        info['hostname'] = socket.gethostname()
-        info['mac-address'] = ':'.join(re.findall('..',
-                                       '%012x' % uuid.getnode()))
-        info['processor'] = platform.processor()
-        info['ram'] = str(
-            round(psutil.virtual_memory().total / (1024.0 ** 3)))+" GB"
-        return info
-    except Exception as e:
-        print(e)
-
-
-print(getSystemInfo())
+users = pwd.getpwall()
+for user in users:
+    print(user.pw_name, user.pw_shell)
